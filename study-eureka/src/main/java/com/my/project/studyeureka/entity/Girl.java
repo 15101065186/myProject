e2d2c712bfc5a5cf46286219cd12edbb029f0d3c
libo <l>
2018-06-14 18:22:56 +0800

    new file

---
+++ study-eureka/src/main/java/com/my/project/studyeureka/entity/Girl.java
@@ -0,0 +1,11 @@
+package com.my.project.studyeureka.entity;
+
+/**
+ * @author: 李波
+ * Mail:        libo@sudiyi.cn
+ * Date:        17:47 06/14/2018
+ * Version:     1.0
+ * Description:
+ */
+public class Girl {
+}
