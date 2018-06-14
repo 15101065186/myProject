a77de9bc479edce81c1c632c46adcc8a8eff7bb7
libo <l>
2018-06-14 16:31:26 +0800

    Eureka-Study

---
+++ study-eureka/src/main/java/com/my/project/studyeureka/StudyEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.my.project.studyeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyEurekaApplication.class, args);
+    }
+}
