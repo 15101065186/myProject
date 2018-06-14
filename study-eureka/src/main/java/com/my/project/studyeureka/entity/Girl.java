f78b9917fc5c17f7710fe6556f02eac6afde19ca
libo <l>
2018-06-14 18:27:53 +0800

    new file

--- study-eureka/src/main/java/com/my/project/studyeureka/entity/Girl.java
+++ study-eureka/src/main/java/com/my/project/studyeureka/entity/Girl.java
@@ -1,5 +1,7 @@
 package com.my.project.studyeureka.entity;
 
+import java.io.Serializable;
+
 /**
  * @author: 李波
  * Mail:        libo@sudiyi.cn
@@ -7,5 +9,15 @@ package com.my.project.studyeureka.entity;
  * Version:     1.0
  * Description:
  */
-public class Girl {
+
+public class Girl implements Serializable {
+    private static final long serialVersionUID = 3965659605053958231L;
+
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private String cupSize;
 }
